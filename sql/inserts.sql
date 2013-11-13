12ea7b0f101238cd18d83ea0779b41f006d34b30
vagrant <vagrant@precise32.(none)>
2013-11-13 06:51:49 +0000

    Rebuild schema templates and add composer requires

---
+++ sql/inserts.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO song
+    (name, artist, file_path)
+    VALUES
+    	('Stairway To Heaven', 'Led Zeppelin', 'files');
+
+INSERT INTO song
+    (name, artist, file_path)
+    VALUES
+    	('November rain','Guns N Roses', 'files');
+
+INSERT INTO song
+    (name, artist, file_path)
+    VALUES
+    	('Crazy Train','Ozzy Osbourne','files');
